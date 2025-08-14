f4812905cc6a441a036ba6b7512d9a5df4e2cc05
dpendleton1 <daniel.pendleton@gmail.com>
2025-08-14 09:33:19 -0400

    added comments

--- settings.R
+++ settings.R
@@ -28,10 +28,10 @@ endYEAR = 2020
 begMONTH = 1
 endMONTH = 12
 
-#season
+#season (monthly binning)
 ssn_beg=rbind(c(1,1), c(2,1), c(3,1), c(4,1), c(5,1), c(6,1), c(7,1), c(8,1), c(9,1), c(10,1), c(11,1), c(12,1))
 ssn_end=rbind(c(1,31), c(2,29), c(3,31), c(4,30), c(5,31), c(6,30), c(7,31), c(8,30), c(9,30), c(10,31), c(11,30), c(12,31) )
 
+#load data
 file_loc = paste0(curr_dir, "/data/", fn)
 dat <- read_csv(file = file_loc)
-
